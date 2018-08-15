af589300c0a335d97554bc817e92ffddaf671be3
Francisco <fvcneto.master98@gmail.com>
2018-08-15 10:45:08 -0300

    mpow code

---
+++ Week-5/francisco/mpow.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#define MAX 51
+#define M 1000000007
+
+void identity(long long a[MAX][MAX]){
+    for(int i = 0; i < MAX; i++)
+        for(int j = 0; j < MAX; j++)
+            a[i][j] = (i==j);
+}
+
+void mmult(long long a[MAX][MAX], long long b[MAX][MAX]){
+    long long temp[MAX][MAX];
+
+    for(int i = 0; i < MAX; i++)
+        for(int j = 0; j < MAX; j++){
+            temp[i][j] = 0;
+            for(int k = 0; k < MAX; k++)
+                temp[i][j] = (temp[i][j] + ((a[i][k] * b[k][j])%M))%M;
+        }
+    for(int i = 0; i < MAX; i++)
+        for(int j = 0; j < MAX; j++)
+            a[i][j] = temp[i][j];
+}
+
+void mexpo(long long res[MAX][MAX], long long a[MAX][MAX], int n){
+    identity(res);
+
+    while(n > 0){
+        if(n%2)
+            mmult(res, a);
+        mmult(a,a);
+        n/=2;
+    }
+}
+
+int main(){
+
+    int t, m, n, i, j;
+    scanf("%d", &t);
+
+    long long matrix[MAX][MAX];
+    long long res[MAX][MAX];
+    
+    while(t--){
+        scanf("%d%d", &m,&n);
+
+        for(i = 0; i < m; i++)
+            for(j = 0; j < m; j++)
+                scanf("%lld", &matrix[i][j]);
+
+        mexpo(res, matrix, n);
+
+        for(i = 0; i < m; i++)
+            for(j = 0; j < m; j++){
+                if((j+1)%m)
+                    printf("%lld ", res[i][j]);
+                else
+                    printf("%lld\n", res[i][j]);
+            }
+    }
+    
+    return 0;
+}
