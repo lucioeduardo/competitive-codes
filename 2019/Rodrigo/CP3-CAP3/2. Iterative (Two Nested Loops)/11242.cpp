4e289be772905088a6e35f5f0cac2ce30fc22eb7
08pixels <luizrodrigomarques@gmail.com>
2019-03-02 14:24:01 -0300

    arg

--- 2019/Rodrigo/CP3-CAP3/2. Iterative (Two Nested Loops)/11242.cpp
+++ 2019/Rodrigo/CP3-CAP3/2. Iterative (Two Nested Loops)/11242.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 double F[15], R[15];
-double ratio[105];
+double calc[105];
 
 int main() {
     int f, r;
@@ -17,13 +17,13 @@ int main() {
 
         for(int i=0; i<f; ++i)
             for(int j=0; j<r; ++j)
-                ratio[i * r + j] = R[j]/F[i];
+                calc[i * r + j] = R[j]/F[i];
 
-        sort(ratio, ratio+r*f);
+        sort(calc, calc+r*f);
         double answer = 0;
 
         for(int i=1; i < r*f; ++i)
-            answer = max(answer, ratio[i]/ratio[i-1]);
+            answer = max(answer, calc[i]/calc[i-1]);
 
         cout << fixed << setprecision(2) << answer << endl;
     }
