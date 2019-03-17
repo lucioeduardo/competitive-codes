93b86a12c2f656e252411af1774db1126486198e
Rodrigo Marques <luizrodrigomarques@gmail.com>
2019-03-17 16:23:26 -0300

    416 - LED test

--- 2019/Rodrigo/CP3-CAP3/8. Recursive Backtracking (Harder)/00416.cpp
+++ 2019/Rodrigo/CP3-CAP3/8. Recursive Backtracking (Harder)/00416.cpp
@@ -3,55 +3,40 @@ using namespace std;
 
 int n;
 string display[15];
-string key[15];
 unordered_map<string, int> number;
 
 
-bool solve(int ind, int last, string turnOff) {
+bool solve(int ind, int last, int burned) {
 
-    if(ind == n) {
-        for(int i=1; i<n; ++i) {
-            if((number[display[i - 1]] - number[display[i]]) != 1)
-                return false;
-        }
-
+    if(ind == n)
         return true;
-    }
 
     for(auto &num: number) {
 
+        if((last != -1) && (last - num.second) != 1)
+            continue;
+
         bool testNumber = true;
+        int off = 0;
 
-        for(int i=0; i<7; ++i) {
-            if((display[ind][i] == 'Y' && num.first[i] == 'N')) {
+        for(int i=0; i<7 && testNumber; ++i) {
+
+            if(display[ind][i] == 'Y' && (burned & (1 << i)))
                 testNumber = false;
-                break;
-            }
-        }
 
+            else if(display[ind][i] == 'Y' && num.first[i] == 'N')
+                testNumber = false;
+
+            else if(num.first[i] == 'Y' && display[ind][i] == 'N')
+                off |= (1 << i);
+            
+        }
+        
         if(testNumber) {
-            testNumber = false;
-            for(int i=0; i<7; ++i) {
-                if(num.first[i] != turnOff[i])
-                    testNumber = true;
-            }
-        }
-
-        if(testNumber && ((last == -1) || ((last != -1) && ((last-num.second) == 1)) )) {
-            string recover = display[ind];
-            display[ind] = num.first;
-            string off = turnOff;
-
-            for(int i=0; i<7; ++i) {
-                if(num.first[i] != recover[i])
-                    off[i] = 'Y';
-            }
-
-            if(solve(ind + 1, num.second, off))
+            if(solve(ind + 1, num.second, burned | off))
                 return true;
-
-            display[ind] = recover;
         }
+
     }
 
     return false;
@@ -64,12 +49,13 @@ int main() {
     number["NYYNNYY"] = 4; number["YNYYNYY"] = 5;
     number["YNYYYYY"] = 6; number["YYYNNNN"] = 7;
     number["YYYYYYY"] = 8; number["YYYYNYY"] = 9;
-    
+
     while((cin >> n) && n) {
         for(int i=0; i<n; ++i)
             cin >> display[i];
         
-        printf("%s\n", solve(0, -1, "NNNNNNN") ? "MATCH" : "MISMATCH");
+        printf("%s\n", solve(0, -1, 0) ? "MATCH" : "MISMATCH");
     }
+
     return 0;
 }
