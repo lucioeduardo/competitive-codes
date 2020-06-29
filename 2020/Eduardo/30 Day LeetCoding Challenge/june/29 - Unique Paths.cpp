471b14e07921480119d4093d7a6320f9c26ed8b2
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-06-29 12:29:40 -0300

    LeetCode june - day 29

---
+++ 2020/Eduardo/30 Day LeetCoding Challenge/june/29 - Unique Paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m+2][n+2];
+        
+        memset(dp,0,sizeof(dp));
+        
+        dp[m+1][n]=1;
+        
+        for(int i=m; i>=1; i--){
+            for(int j=n; j>=1; j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        
+        return dp[1][1];
+    }
+};
