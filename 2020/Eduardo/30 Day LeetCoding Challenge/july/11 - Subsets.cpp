7251ba949a44ce1cb3c28cba520ee6f324318c8f
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-07-11 10:36:24 -0300

    LeetCode july - day 11

---
+++ 2020/Eduardo/30 Day LeetCoding Challenge/july/11 - Subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        
+        int n = nums.size();
+        
+        for(int i=0; i<(1<<n); i++){
+            vector<int> at;
+            for(int j=0; j<n; j++){
+                if((1<<j) & i) at.push_back(nums[j]);
+            }
+            
+            res.push_back(at);
+        }
+        
+        return res;
+    }
+};
