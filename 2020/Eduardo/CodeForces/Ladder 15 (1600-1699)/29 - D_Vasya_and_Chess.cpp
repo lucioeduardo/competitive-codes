8eedb48f1d0f3fab1f40d9dc2820b5c4afe35936
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-08-21 14:22:22 -0300

    CodeForces #029

---
+++ 2020/Eduardo/CodeForces/Ladder 15 (1600-1699)/29 - D_Vasya_and_Chess.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+
+  cin>>n;
+
+  if(n%2 == 0){
+    cout <<  "white\n1 2" << endl;
+  }else{
+    cout << "black" << endl;
+  }
+
+  return 0;
+}
