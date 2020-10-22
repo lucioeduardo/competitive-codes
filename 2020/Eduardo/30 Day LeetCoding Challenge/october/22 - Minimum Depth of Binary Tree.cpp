6dadc8447ee28718b295216a70c2e098531dc915
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-10-22 08:32:20 -0300

    LeetCode october - day 22

---
+++ 2020/Eduardo/30 Day LeetCoding Challenge/october/22 - Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        
+        if(!root->left && !root->right) return 1;
+        
+        int res = 99999999;
+        
+        if(root->left) res = min(res,minDepth(root->left));
+        if(root->right) res = min(res,minDepth(root->right));
+        
+        return 1+res;
+    }
+};
