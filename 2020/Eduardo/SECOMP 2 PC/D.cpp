660dc950e7ec99248666f3bc964753796c070920
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-10-24 17:35:19 -0300

    Secomp 2 PC - D

---
+++ 2020/Eduardo/SECOMP 2 PC/D.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  double a,b;
+
+  cin>>a>>b;
+
+  printf("%.2lf",a/b);
+  
+  return 0;
+}
