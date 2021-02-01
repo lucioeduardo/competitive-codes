bfc2769da2de613cf51097149f1ac67e7231fc73
jadsonlucio <jadsonaluno@hotmail.com>
2021-02-01 18:56:04 -0300

    add competitive programing 3 book problems

---
+++ Competitive programing 3/chapter-1/Abridged_Problem.py
@@ -0,0 +1,32 @@
+min_cost = 1e100
+best_solution = None
+
+def dist(p1, p2):
+    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+
+def find_best_config(coordinates):
+    if len(coordinates) == 2:
+        return dist(*coordinates), coordinates
+
+    coordinate0 = coordinates.pop(0)
+    best_score = 1e100
+    best_config = None
+    for i in range(len(coordinates)):
+        copy = coordinates[:]
+        coordinate1 = copy.pop(i)
+        dist_c = dist(coordinate0, coordinate1)
+        dist_t, config = find_best_config(copy)
+        if dist_c + dist_t < best_score:
+            best_score = dist_c + dist_t
+            best_config = [coordinate0, coordinate1] +  config
+
+    return best_score, best_config
+
+
+n = int(input())
+coordinates = []
+
+for i in range(2*n):
+    coordinates.append(list(map(int, input().split())))
+
+print(round(find_best_config(coordinates)[0], 2))
