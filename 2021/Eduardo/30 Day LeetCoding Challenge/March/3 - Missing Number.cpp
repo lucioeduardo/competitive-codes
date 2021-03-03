509e33c0b396628511d0837448cdf14e9560bbf9
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-03-03 08:37:24 -0300

    LeetCode march - day 3

---
+++ 2021/Eduardo/30 Day LeetCoding Challenge/March/3 - Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        
+        for(int x: nums){
+            sum -= x;
+        }
+        
+        return sum;
+    }
+};
