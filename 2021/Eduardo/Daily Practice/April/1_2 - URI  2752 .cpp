101dec38640f9e497ab4f7261eb6ce1140673656
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-01 09:04:09 -0300

    Practice april - day 1, Q2

---
+++ 2021/Eduardo/Daily Practice/April/1_2 - URI  2752 .cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  char str[50] = "AMO FAZER EXERCICIO NO URI";
+  
+  printf("<%s>\n", str);
+  printf("<%30s>\n", str);
+  printf("<%.20s>\n", str);
+  printf("<%-20s>\n", str);
+  printf("<%-30s>\n", str);
+  printf("<%.30s>\n", str);
+  printf("<%30.20s>\n", str);
+  printf("<%-30.20s>\n", str);
+
+  return 0;
+}
