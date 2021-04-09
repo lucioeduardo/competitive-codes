2ce682138178e25a539d91ed5cc0d15f8d826d97
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-09 08:20:32 -0300

    Practice april, day 9, Q2

---
+++ 2021/Eduardo/Daily Practice/April/9_2 - URI 2754.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  double a = 234.345;
+  double b = 45.698;
+
+  printf("%.6lf - %.6lf\n",a,b);
+  printf("%.0lf - %.0lf\n",a,b);
+  printf("%.1lf - %.1lf\n",a,b);
+  printf("%.2lf - %.2lf\n",a,b);
+  printf("%.3lf - %.3lf\n",a,b);
+  printf("%e - %e\n",a,b);
+  printf("%E - %E\n",a,b);
+  printf("%.3lf - %.3lf\n",a,b);
+  printf("%.3lf - %.3lf\n",a,b);
+  
+  return 0;
+}
