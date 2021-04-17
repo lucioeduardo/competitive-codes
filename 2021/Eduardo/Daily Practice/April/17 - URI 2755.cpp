25f9ba8837ca9349005e74bc19b0e60e2b840bda
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-17 09:28:26 -0300

    Practice april, day 17, Q1

---
+++ 2021/Eduardo/Daily Practice/April/17 - URI 2755.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  cout << "\"Ro\'b\'er\tto\\/\"" << endl;
+  cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << endl;
+  cout << "(^_-) (-_-) (-_^)" << endl;
+  cout << "(\"_\") (\'.\')" << endl;
+  
+  return 0;
+}
