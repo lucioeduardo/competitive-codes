4d602705845c5e8c1442aeac64c13799b4a0debb
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-17 09:28:38 -0300

    Practice april, day 17, Q2

---
+++ 2021/Eduardo/Daily Practice/April/17_2 - URI 2987.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  char c;
+  
+  cin>>c;
+  cout << (c-'A'+1) << endl;
+  
+  return 0;
+}
