9eb10dd0e1da57417204d70d023f68ad850c2326
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-27 08:52:39 -0300

    Practice april, day 27, Q2

---
+++ 2021/Eduardo/Daily Practice/April/27_2 - URI 2950.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  double a,b,c;
+
+  cin>>a>>b>>c;
+
+  printf("%.2lf\n",a/(b+c));
+  
+  return 0;
+}
