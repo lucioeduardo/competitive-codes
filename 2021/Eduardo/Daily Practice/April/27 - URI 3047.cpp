8123b56ccc79674afe2457a865472e8183c90135
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-04-27 08:52:34 -0300

    Practice april, day 27, Q1

---
+++ 2021/Eduardo/Daily Practice/April/27 - URI 3047.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  int a,b,c;
+
+  cin>>a>>b>>c;
+
+  int d = a-(b+c);
+
+  cout << max(b,max(c,d)) << endl;
+  
+  return 0;
+}
