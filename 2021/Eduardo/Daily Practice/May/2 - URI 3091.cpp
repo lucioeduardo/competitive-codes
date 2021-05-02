b22a390185f33b2ab52dc575d2c49a6ac5ddd012
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-05-02 09:50:04 -0300

    Practice may, day 2, Q1

---
+++ 2021/Eduardo/Daily Practice/May/2 - URI 3091.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  int a,b;
+
+  cin>>a>>b;
+
+  cout << a%b << endl;
+  
+  return 0;
+}
