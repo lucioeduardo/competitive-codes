8209d021fc38a9a23b3939196f3b7b425a2830d8
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-05-05 11:49:22 -0300

    Practice may, day 5, Q1

---
+++ 2021/Eduardo/Daily Practice/May/5 - URI 1893.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  int a,b;
+
+  cin>>a>>b;
+
+  if(b <= 2) cout << "nova" << endl;
+  else if(b >= 97) cout << "cheia" << endl;
+  else if(a > b) cout << "minguante" << endl;
+  else cout << "crescente" << endl;
+
+  return 0;
+}
