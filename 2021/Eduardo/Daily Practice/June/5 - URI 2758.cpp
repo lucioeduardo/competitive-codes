578b2da3441dd7ef0ad79e924c1e105d6cb67a5f
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-06-05 13:18:01 -0300

    Practice june. day 5, Q1

---
+++ 2021/Eduardo/Daily Practice/June/5 - URI 2758.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  float a,b;
+  double c,d;
+
+  cin>>a>>b>>c>>d;
+
+  printf("A = %f, B = %f\n",a,b);
+  printf("C = %f, D = %f\n",c,d);
+  printf("A = %.1f, B = %.1f\n",a,b);
+  printf("C = %.1lf, D = %.1lf\n",c,d);
+  printf("A = %.2f, B = %.2f\n",a,b);
+  printf("C = %.2lf, D = %.2lf\n",c,d);
+  printf("A = %.3f, B = %.3f\n",a,b);
+  printf("C = %.3lf, D = %.3lf\n",c,d);
+  printf("A = %.3E, B = %.3E\n",a,b);
+  printf("C = %.3E, D = %.3E\n",c,d);
+  printf("A = %.0f, B = %.0f\n",a,b);
+  printf("C = %.0lf, D = %.0lf\n",c,d);
+
+  return 0;
+}
