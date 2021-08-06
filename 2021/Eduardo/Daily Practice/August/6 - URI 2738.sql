4176b0f42438e78d9f34af376f0e843e1c86c0c3
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-08-06 09:13:24 -0300

    Practice august, day 6

---
+++ 2021/Eduardo/Daily Practice/August/6 - URI 2738.sql
@@ -0,0 +1,7 @@
+SELECT
+	candidate.name, ROUND((score.math*2 + score.specific*3 + project_plan*5)/10,2) as avg
+FROM
+	candidate INNER JOIN score
+ON
+	candidate.id = score.candidate_id
+ORDER BY avg DESC
