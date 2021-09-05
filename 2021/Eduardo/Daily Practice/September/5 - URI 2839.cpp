18c67acb475291c01969145310e4148afd6398fb
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-09-05 11:30:42 -0300

    Practice september, day 5

---
+++ 2021/Eduardo/Daily Practice/September/5 - URI 2839.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  int v;
+  cin>>v;
+
+  cout << v+1 << endl;
+  
+  return 0;
+}
