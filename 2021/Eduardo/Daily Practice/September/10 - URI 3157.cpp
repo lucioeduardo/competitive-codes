2129c7192060684285b0c2daa0c1a0c1b47e24c5
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-09-10 14:28:20 -0300

    Practice september, day 10

---
+++ 2021/Eduardo/Daily Practice/September/10 - URI 3157.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  string s;
+
+  while(cin>>s);
+
+  cout << "Nao se refuta ciencia com opiniao" << endl;
+  
+  return 0;
+}
