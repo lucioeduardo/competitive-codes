670ec165af1abb4f163c63323f95e914b6417eb8
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2021-09-13 11:18:05 -0300

    Practice september, day 13

---
+++ 2021/Eduardo/Daily Practice/September/13 - URI 3303.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define init_arr(arr,val) memset(arr,val,sizeof(arr))
+#define ll long long
+using namespace std;
+
+int main(){
+  string s;
+
+  cin>>s;
+
+  if(s.length() >= 10) cout << "palavrao" << endl;
+  else cout << "palavrinha" << endl;
+  
+  return 0;
+}
